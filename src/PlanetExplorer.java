ed1f1c1a6e555ff3241c1dc7a4b1096acc175e3c
somename <someemail>
2017-06-09 12:53:53 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 // Before you commit and push write your student ID and finish time here.
 // Finish time:
@@ -10,7 +12,8 @@ public class PlanetExplorer {
 	private String direction = "n";
 	private int roverX = 0;
 	private int roverY = 0;
-
+	private List<int[]> listaPrepreka = new ArrayList<int[]>();
+	private List<int[]> listaKoriscenihPrepreka = new ArrayList<int[]>();
 	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
@@ -37,6 +40,28 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		if (obstacles != null){
+			char[] nizPrepreka = obstacles.toCharArray();
+			int counter = 0;
+			int preprekaX = 0;
+			int preprekaY = 0;
+			int koriscena = 0;
+			for (int i = 0; i < nizPrepreka.length; i++){
+				char karakter = nizPrepreka[i];
+				if (Character.isDigit(karakter)){
+					if (counter == 0){
+						preprekaX = Character.getNumericValue(karakter);
+						counter++;
+					} else if (counter == 1){
+						counter = 0;
+						preprekaY = Character.getNumericValue(karakter);
+						int[] prepreka = {preprekaX, preprekaY, koriscena};
+						listaPrepreka.add(prepreka);
+					}
+				}
+			}
+		}
+		
 		char[] niz = command.toCharArray();
 		for (char i : niz){
 			switch(i){
@@ -79,17 +104,61 @@ public class PlanetExplorer {
 	
 	public void moveForth(){
 		switch(direction){
-			case "n": 
-				if (roverY == y - 1)
+			case "n":
+				if (roverY == y - 1){
+					if (obstacles != null){
+						for (int[] i : listaPrepreka){
+							if (i[0] == roverX && i[1] == 0){
+								if (i[2] != 1){
+									i[2] = 1;
+									listaKoriscenihPrepreka.add(i);
+								}
+								return;
+							}
+						}
+					}
 					roverY = 0;
-				else
+				}
+				else {
+					if (obstacles != null){
+						for (int[] i : listaPrepreka){
+							if (i[0] == roverX && i[1] == (y + 1)){
+								if (i[2] != 1){
+									i[2] = 1;
+									listaKoriscenihPrepreka.add(i);
+								}
+								return;
+							}
+						}
+					}
 					roverY++; 
+				}
 				break;
 			case "e": 
-				if (roverX == x - 1)
+				if (roverX == x - 1){
+					for (int[] i : listaPrepreka){
+						if (i[0] == 0 && i[1] == roverY){
+							if (i[2] != 1){
+								i[2] = 1;
+								listaKoriscenihPrepreka.add(i);
+							}
+							return;
+						}
+					}
 					roverX = 0;
-				else
+				}
+				else {
+					for (int[] i : listaPrepreka){
+						if (i[0] == (x + 1) && i[1] == roverY){
+							if (i[2] != 1){
+								i[2] = 1;
+								listaKoriscenihPrepreka.add(i);
+							}
+							return;
+						}
+					}
 					roverX++; 
+				}	
 				break;
 			case "s": 
 				if (roverY == 0)
